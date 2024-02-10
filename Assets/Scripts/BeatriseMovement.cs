ce026e395d4b58d2f9158ca339d61f3ed35bcb50
RevnaEir <beatriseleonovica@gmail.com>
2024-02-10 15:49:28 +0200

    MAin scene
    salabojam zemi un beatrise kustas

---
+++ Assets/Scripts/BeatriseMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class BeatriseMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust the speed of movement
+    public float moveTime = 4f;
+    private bool movingRight = true; // Initial movement direction
+    private Rigidbody2D rb;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(ChangeDirection());
+    }
+
+    void Update()
+    {
+        if (movingRight)
+        {
+            rb.velocity = new Vector2(moveSpeed, Physics.gravity.y);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, Physics.gravity.y);
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveTime); // 
+            movingRight = !movingRight; // Change direction
+        }
+    }
+}
